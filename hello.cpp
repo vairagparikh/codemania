363aee75043650caba098171e3c7362bf4533d89
vairag <vairagparikh70@gmail.com>
2022-01-27 17:02:06 -0500

    Updating Skeleton File

--- hello.cpp
+++ hello.cpp
@@ -4,15 +4,13 @@ using namespace std;
 int main() {
 
     // For File IO**************************
+    //Input File Name : input.txt
+    //Output File Name : output.txt
+
     // #ifndef ONLINE_JUDGE
     //     freopen("input.txt","r",stdin);
     //     freopen("output.txt","w",stdout);
     // #endif
-
-    int a,b;
-    cout<<"Welcome to CPP2"<<endl;
-    cin>>a>>b;
-    cout<<a+b;
 
     return 0;
 }
