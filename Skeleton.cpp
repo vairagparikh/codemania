97b8648b7b98415e1e1db9073f232e1e110790cd
vairag <vairagparikh70@gmail.com>
2022-01-27 17:03:48 -0500

    skeleton

---
+++ Skeleton.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // For File IO**************************
+    //Input File Name : input.txt
+    //Output File Name : output.txt
+
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt","r",stdin);
+    //     freopen("output.txt","w",stdout);
+    // #endif
+
+    return 0;
+}
